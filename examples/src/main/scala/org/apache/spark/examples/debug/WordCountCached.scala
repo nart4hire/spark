2ccfe17d9ab9ede9a1f0fcad2579d56990ec6451
mrafid01 <112873399+mrafid01@users.noreply.github.com>
2023-03-04 12:47:04 +0700

    add examples

---
+++ examples/src/main/scala/org/apache/spark/examples/debug/WordCountCached.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+package org.apache.spark.examples.debug
+
+// import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+object WordCountCached{
+    def main(args: Array[String]): Unit = {
+        val spark = SparkSession
+            .builder
+            .appName("WordCountCached")
+            .getOrCreate()
+        val input = "data/wordcount.txt"
+        val data = spark.sparkContext.textFile(input)
+        val words = data.flatMap(x => x.split(" "))
+        words.cache()
+        println(s"Words Count = ${words.count()}")
+        val pairs = words.map((_, 1))
+        val result = pairs.reduceByKey(_ + _)
+        result.cache()
+        println(s"Result Count = ${result.count()}")
+        words.unpersist()
+        val output = result.collect()
+        output.foreach{
+            case(key, value) => println(s"$key : $value")
+        }
+        println(s"Result Take 10 = ${result.take(10)}")
+        result.unpersist()
+        spark.stop()
+    }
+}
+// scalastyle:on println
